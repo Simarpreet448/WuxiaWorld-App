c1c7836626045ce566ce5e424f0121b973fecd6b
Simarpreet448 <simarpreetsingh448@gmail.com>
2021-06-21 09:43:51 +1200

    added some more stuff

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.finalproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void onClickWuxia(View view) {
+        Intent categoryListActivity = new Intent(this, SecondActivity.class);
+        categoryListActivity.putExtra("Genre", "Wuxia (Martial Heroes)");
+        startActivity(categoryListActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
--- app/src/main/java/com/example/finalproject/WebNovelJavaFile/WebNovel.java
+++ app/src/main/java/com/example/finalproject/WebNovelJavaFile/WebNovel.java
@@ -1,4 +1,4 @@
-package com.example.finalproject;
+package com.example.finalproject.WebNovelJavaFile;
 
 public class WebNovel {
     private String name;
