2c5b0770db537716f8b410d2b559ddb1e9c00859
Simarpreet448 <simarpreetsingh448@gmail.com>
2021-06-21 09:15:29 +1200

    updated

---
+++ app/src/main/java/com/example/finalproject/WebNovel.java
@@ -0,0 +1,37 @@
+package com.example.finalproject;
+
+public class WebNovel {
+    private String name;
+    private String desciption;
+    private int image;
+
+    public WebNovel(String name, String desciption, int image) {
+        this.name = name;
+        this.desciption = desciption;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
