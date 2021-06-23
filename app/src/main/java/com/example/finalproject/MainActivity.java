143c87944e02912f1b47b8513bcb1d723204ea4e
Simarpreet448 <simarpreetsingh448@gmail.com>
2021-06-23 16:16:51 +1200

    happy with progress bar. but there is still some lag when clicking novel categories for the first time

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,47 +6,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActivityOptions;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressDialog progress;
+    Thread tread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         progress = new ProgressDialog(MainActivity.this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onClickText(View view)
     {
-                progress.setMessage("Please Wait Loading...");
-                progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                progress.setIndeterminate(true);
-                progress.show();
-        new Thread()
-        {
-            public void run()
-            {
-                Intent categoryListActivity = new Intent(MainActivity.this, SecondActivity.class);
-                switch (view.getId())
-                {
-                    case R.id.wuxiatextview:
-                        categoryListActivity.putExtra("Genre", "Wuxia (Martial Heroes)");
-                        break;
-                    case R.id.xianxiatextview:
-                        categoryListActivity.putExtra("Genre", "Xianxia (Immortal Heroes)");
-                        break;
-                    case R.id.xuanhuantextview:
-                        categoryListActivity.putExtra("Genre", "Xuanhuan (Mysterious Fantasy)");
-                        break;
+        final ProgressDialog dialog= ProgressDialog.show(this,"Loading", "Please wait....",true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Intent categoryListActivity = new Intent(MainActivity.this, SecondActivity.class);
+                    switch (view.getId())
+                    {
+                        case R.id.wuxiatextview:
+                            categoryListActivity.putExtra("Genre", "Wuxia (Martial Heroes)");
+                            break;
+                        case R.id.xianxiatextview:
+                            categoryListActivity.putExtra("Genre", "Xianxia (Immortal Heroes)");
+                            break;
+                        case R.id.xuanhuantextview:
+                            categoryListActivity.putExtra("Genre", "Xuanhuan (Mysterious Fantasy)");
+                            break;
+                    }
+                    startActivity(categoryListActivity);
+                    dialog.dismiss();
                 }
-                startActivity(categoryListActivity);
-                progress.dismiss();
+                catch(Exception ex)
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Something went wrong. Please try again later", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
-        }.start();
+        }).start();
     }
 }
