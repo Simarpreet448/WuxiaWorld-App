cd4da3ac17288dc305e5e3b2fbf638ca1f51a627
Simarpreet448 <simarpreetsingh448@gmail.com>
2021-06-23 13:10:18 +1200

    new jpg images work and are light weight compared to png and the progress bar shows for a very little time. needs improvement

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,21 +4,49 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        progress = new ProgressDialog(MainActivity.this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClickWuxia(View view) {
-        Intent categoryListActivity = new Intent(MainActivity.this, SecondActivity.class);
-        categoryListActivity.putExtra("Genre", "Wuxia (Martial Heroes)");
-        startActivity(categoryListActivity);
+    public void onClickText(View view)
+    {
+                progress.setMessage("Please Wait Loading...");
+                progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progress.setIndeterminate(true);
+                progress.show();
+        new Thread()
+        {
+            public void run()
+            {
+                Intent categoryListActivity = new Intent(MainActivity.this, SecondActivity.class);
+                switch (view.getId())
+                {
+                    case R.id.wuxiatextview:
+                        categoryListActivity.putExtra("Genre", "Wuxia (Martial Heroes)");
+                        break;
+                    case R.id.xianxiatextview:
+                        categoryListActivity.putExtra("Genre", "Xianxia (Immortal Heroes)");
+                        break;
+                    case R.id.xuanhuantextview:
+                        categoryListActivity.putExtra("Genre", "Xuanhuan (Mysterious Fantasy)");
+                        break;
+                }
+                startActivity(categoryListActivity);
+                progress.dismiss();
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/finalproject/SecondActivity.java
+++ app/src/main/java/com/example/finalproject/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.GridView;
 
@@ -13,7 +14,7 @@ import java.util.List;
 
 import com.example.finalproject.WebNovelJavaFile.CustomAdaptor;
 public  class SecondActivity extends AppCompatActivity {
-
+    private String Genre;
     @Override
     protected void onCreate(Bundle savedInstanceState)
         {
@@ -21,11 +22,29 @@ public  class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
 
         List<WebNovel> itemsList = new ArrayList<>();
-        itemsList.add(new WebNovel("A Will Ethernal","A Will Eternal is the fourth major xianxia novel by the renowned Er Gen. It tells the tale of Bai Xiaochun, an endearing but exasperating young man who is driven primarily by his fear of death and desire to live forever, but who deeply values friendship and family. The story takes place in the same Universe as Er Gen's other novels, but is a separate story and can be read alone. That said, it does have connections to the other novels, especially I Shall Seal the Heavens. Those connections only pertain to a handful of chapters, so you can read the novel as a whole and enjoy it without first reading ISSTH. However, the best reading experience would begin with the later portions of ISSTH. This novel is much more comedic and light-hearted than Er Gen's previous works, although it has its fair share of action and drama.", R.raw.awillethernal,R.string.xianxiaimmortalheroes));
-        itemsList.add(new WebNovel("Renegade Immortal","nothing",R.raw.renegateimmortal, R.string.xianxiaimmortalheroes));
+        Intent intent = getIntent();
+
+        Genre = intent.getStringExtra("Genre");
+            getSupportActionBar().setTitle(Genre);
+            if(Genre.equals("Xianxia (Immortal Heroes)"))
+            {
+                itemsList.add(new WebNovel("A Will Ethernal","A Will Eternal is the fourth major xianxia novel by the renowned Er Gen. It tells the tale of Bai Xiaochun, an endearing but exasperating young man who is driven primarily by his fear of death and desire to live forever, but who deeply values friendship and family. The story takes place in the same Universe as Er Gen's other novels, but is a separate story and can be read alone. That said, it does have connections to the other novels, especially I Shall Seal the Heavens. Those connections only pertain to a handful of chapters, so you can read the novel as a whole and enjoy it without first reading ISSTH. However, the best reading experience would begin with the later portions of ISSTH. This novel is much more comedic and light-hearted than Er Gen's previous works, although it has its fair share of action and drama.", R.raw.awillethernal,R.string.xianxiaimmortalheroes));
+                itemsList.add(new WebNovel("Renegade Immortal","nothing",R.raw.renegateimmortal, R.string.xianxiaimmortalheroes));
+            }
+            else if(Genre.equals("Wuxia (Martial Heroes)"))
+            {
+                itemsList.add(new WebNovel("A Will Ethernal","A Will Eternal is the fourth major xianxia novel by the renowned Er Gen. It tells the tale of Bai Xiaochun, an endearing but exasperating young man who is driven primarily by his fear of death and desire to live forever, but who deeply values friendship and family. The story takes place in the same Universe as Er Gen's other novels, but is a separate story and can be read alone. That said, it does have connections to the other novels, especially I Shall Seal the Heavens. Those connections only pertain to a handful of chapters, so you can read the novel as a whole and enjoy it without first reading ISSTH. However, the best reading experience would begin with the later portions of ISSTH. This novel is much more comedic and light-hearted than Er Gen's previous works, although it has its fair share of action and drama.", R.raw.awillethernal,R.string.xianxiaimmortalheroes));
+                itemsList.add(new WebNovel("Renegade Immortal","nothing",R.raw.courtlady, R.string.xianxiaimmortalheroes));
+            }
+            else if(Genre.equals("Xuanhuan (Mysterious Fantasy)"))
+            {
+                itemsList.add(new WebNovel("A Will Ethernal","A Will Eternal is the fourth major xianxia novel by the renowned Er Gen. It tells the tale of Bai Xiaochun, an endearing but exasperating young man who is driven primarily by his fear of death and desire to live forever, but who deeply values friendship and family. The story takes place in the same Universe as Er Gen's other novels, but is a separate story and can be read alone. That said, it does have connections to the other novels, especially I Shall Seal the Heavens. Those connections only pertain to a handful of chapters, so you can read the novel as a whole and enjoy it without first reading ISSTH. However, the best reading experience would begin with the later portions of ISSTH. This novel is much more comedic and light-hearted than Er Gen's previous works, although it has its fair share of action and drama.", R.raw.awillethernal,R.string.xianxiaimmortalheroes));
+                itemsList.add(new WebNovel("Renegade Immortal","nothing",R.raw.renegateimmortal, R.string.xianxiaimmortalheroes));
+            }
 
         GridView gridView = findViewById(R.id.gridview);
         CustomAdaptor customAdapter = new CustomAdaptor(this, R.layout.webnovellistitem, itemsList);
         gridView.setAdapter(customAdapter);
     }
+
 }
